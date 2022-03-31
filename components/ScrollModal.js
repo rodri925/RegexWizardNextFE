d81cbd8d9eea70475550e3b3e5f6a04f03d32dba
rodri925 <malik.rodriguez@gmail.com>
2022-03-31 15:22:34 -0500

    minor changes to Dash css, started on edit/add/del functionality

--- components/ScrollModal.js
+++ components/ScrollModal.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from '../styles/Dashboard.module.css';
 
 const ScrollModal = ({
-  regex, user, desc, setToggleModal,
-}) => (
-  <div className={styles.scrollModal}>
-    <button type="submit" onClick={() => setToggleModal(false)}>X</button>
-    <h3>
-      Regex:&nbsp;
-      {regex}
-    </h3>
-    <p>
-      Description:&nbsp;
-      {desc}
-    </p>
-    <span>
-      User:&nbsp;
-      {user}
-    </span>
-  </div>
-);
+  title, regex, user, desc, id, setToggleModal, addModal, editModal, scrollExists,
+}) => {
+  const [currentRegex, setCurrentRegex] = useState(regex);
+  const [currentDesc, setCurrentDesc] = useState(desc);
+  const [currentTitle, setCurrentTitle] = useState(title);
+
+  return (
+    <div className={styles.scrollModal}>
+      <button type="submit" onClick={() => setToggleModal(false)}>X</button>
+      <span>Title</span>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setCurrentTitle(e.target.value)}
+      />
+      <span>Regex</span>
+      <input
+        type="text"
+        value={regex}
+        onChange={(e) => setCurrentRegex(e.target.value)}
+      />
+      <span>Description</span>
+      <textarea
+        name="modalDesc"
+        placeholder={desc}
+        onChange={(e) => setCurrentDesc(e.target.value)}
+      />
+      <div className={styles.modalBtnWrapper}>
+        {!scrollExists
+        && (<button type="submit" className={styles.modalBtn} onClick={() => addModal(currentRegex, user, currentDesc, currentTitle)}>Add</button>)}
+        {scrollExists
+        && (<button type="submit" className={styles.modalBtn} onClick={() => editModal(currentRegex, user, currentDesc, currentTitle, id)}>Edit</button>)}
+      </div>
+    </div>
+  );
+};
 
 ScrollModal.propTypes = {
+  title: PropTypes.string.isRequired,
   regex: PropTypes.string.isRequired,
   user: PropTypes.string.isRequired,
   desc: PropTypes.string.isRequired,
   setToggleModal: PropTypes.func.isRequired,
+  addModal: PropTypes.func.isRequired,
+  editModal: PropTypes.func.isRequired,
+  scrollExists: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
 };
 export default ScrollModal;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,55 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ScrollModal from '../components/ScrollModal';
 import styles from '../styles/Dashboard.module.css';
 
 const Dashboard = () => {
-  /* 1. Replace use state nums with completed missions pulled from DB when ready
-     2. Need to pull a the first incomplete lesson and first incomplete mission.
-     Must render something different if all lessons/all mission complete.
-     3. Need to come back and make notification for deletion. Cannot use Windows confirm.
-     4. Need to make Components for mission and lesson squares.
-     5. They must be dynamic and  recognize when all content has been compl
-   */
-  const [lessonPercent, setLessonPercent] = useState(() => 7 * 4);
-  const [missionPercent, setMissionPercent] = useState(() => 10 * 4);
-  const [toggleModal, setToggleModal] = useState(false);
-  const [currentScroll, setCurrentScroll] = useState(null);
-  const [user, setUser] = useState('Mickey');
-  const [category, setCategory] = useState('all scrolls');
-
   const [fakeScrolls, setFakeScrolls] = useState([
     {
+      title: 'Vowels',
       user: 'Mickey',
       regex: '[aeioasdfasfasdfasdfdasfasdfasdfadsfasdfasu]',
       desc: 'this regex finds vowels',
       id: 1,
     },
     {
+      title: 'Ugly Finder',
       user: 'Borby',
       regex: '/ugly/',
       desc: 'this regex finds the ugly',
       id: 2,
     },
     {
+      title: 'Okayge',
       user: 'Egg',
       regex: '...',
       desc: 'this regex finds 3 letter words',
       id: 3,
     },
     {
+      title: 'Vowels',
       user: 'Mickey',
       regex: '[aeiou]',
       desc: 'this regex finds vowels',
       id: 4,
     },
     {
+      title: 'Ugly Finder',
       user: 'Borby',
       regex: '/ugly/',
       desc: 'this regex finds the ugly',
       id: 5,
     },
     {
+      title: 'Okayge',
       user: 'Egg',
       regex: '...',
       desc: 'this regex finds 3 letter words',
@@ -57,140 +49,169 @@ const Dashboard = () => {
     },
   ]);
 
-  const [filteredScrolls, setFilteredScrolls] = useState(fakeScrolls);
+  const [toggleModal, setToggleModal] = useState(false);
+  const [selectedScroll, setSelectedScroll] = useState({
+    title: 'N/A',
+    user: 'N/A',
+    regex: 'N/A',
+    desc: 'INIT',
+    id: 0,
+  });
 
-  const setupModal = (event, scroll) => {
-    event.preventDefault();
-    setToggleModal(true);
-    setCurrentScroll(scroll);
+  const scrollSelector = ({
+    title, user, regex, desc, id,
+  }) => {
+    const newScroll = {
+      title,
+      user,
+      regex,
+      desc,
+      id,
+    };
+    setSelectedScroll(newScroll);
   };
 
-  const filterScrolls = (event, currentCategory, currentScrolls) => {
-    event.preventDefault();
-    if (currentCategory === 'my scrolls') {
-      const myScrolls = currentScrolls.filter((scroll) => scroll.user === user);
-      setFilteredScrolls(myScrolls);
-    } else if (currentCategory === 'favorited scrolls') {
-      const favScrolls = currentScrolls.filter((scroll) => scroll.user !== user);
-      setFilteredScrolls(favScrolls);
-    } else {
-      setFilteredScrolls(currentScrolls);
-    }
+  const addModal = (title, user, regex, desc, id) => {
+    const newScroll = {
+      title,
+      user,
+      regex,
+      desc,
+      id,
+    };
+
+    const fakeScrollsHolder = fakeScrolls;
+    fakeScrollsHolder.push(newScroll);
+
+    setFakeScrolls(fakeScrollsHolder);
   };
-  /* Need to Fix: must fix state immediately after deleting */
-  const deleteScroll = (event, delScroll) => {
-    event.preventDefault();
-    if (window.confirm(`Delete ${delScroll.regex}?`)) {
-      const newScrolls = fakeScrolls.filter((scroll) => scroll.id !== delScroll.id);
-      setFakeScrolls(newScrolls);
-      filterScrolls(event, category, newScrolls);
-    }
+
+  const editModal = (title, user, regex, desc, id) => {
+    const newScroll = {
+      title,
+      user,
+      regex,
+      desc,
+      id,
+    };
+    const newFakeScrolls = fakeScrolls.filter((scroll) => scroll.id !== id);
+    newFakeScrolls.push(newScroll);
+
+    setFakeScrolls(newFakeScrolls);
   };
+
+  const doesScrollExist = (id) => {
+    const trueScroll = fakeScrolls.filter((scroll) => scroll.id === id);
+    if (trueScroll) {
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (selectedScroll.id !== 0) {
+      setToggleModal(true);
+    }
+  }, [selectedScroll]);
 
   return (
-    <div className={styles.outerContainer}>
-      <h1>Hi User!</h1>
-      <div className={styles.innerContainer}>
-        <div className={styles.info}>
-          <div className={styles.progress}>
-            <span>
-              Training Progress:&nbsp;
-              {lessonPercent}
-              %
-            </span>
-            <div className={styles.BarOuter}>
-              <div className={styles.BarInner} style={{ width: `${lessonPercent}%` }} />
+    <div className={styles.backgroundWrapper}>
+      <div className={styles.topWrapper}>
+        <div className={styles.pieWrapper}>
+          <div className={styles.welcomeWrapper}>
+            <h1>Welcome, User</h1>
+            <span>Track lesson and mission progress.</span>
+            <br />
+            <span>Create and edit scrolls.</span>
+          </div>
+          <div className={styles.pieBlock}>
+            <h2>Completed Lessons: 60%</h2>
+            <hr />
+            <div className={styles.pieCircle}>
+              <div className={styles.innerCircle} />
             </div>
-            <span>
-              Mission Progress:&nbsp;
-              {missionPercent}
-              %
-            </span>
-            <div className={styles.BarOuter}>
-              <div className={styles.BarInner} style={{ width: `${missionPercent}%` }} />
+            <div className={styles.pieCompleteWrapper}>
+              <div className={styles.pieCompleteInnerWrapper}>
+                <span>Complete:</span>
+                <div className={styles.greenDot} />
+              </div>
+              <div className={styles.pieCompleteInnerWrapper}>
+                <span>Incomplete:</span>
+                <div className={styles.grayDot} />
+              </div>
             </div>
           </div>
-          <div className={styles.missionIncompletes}>
-            <div className={styles.incompleteBox}>
-              <p>
-                Start Lesson:
-                <br />
-                Lesson
-              </p>
-              <a href="./Training">Start</a>
+          <div className={styles.pieBlock}>
+            <h2>Completed Missions: 50%</h2>
+            <hr />
+            <div className={styles.pieCircle2}>
+              <div className={styles.innerCircle} />
             </div>
-            <div className={styles.incompleteBox}>
-              <p>
-                Start Mission:
-                <br />
-                Mission
-              </p>
-              <a href="./Missions">Start</a>
+            <div className={styles.pieCompleteWrapper}>
+              <div className={styles.pieCompleteInnerWrapper}>
+                <span>Complete:</span>
+                <div className={styles.greenDot} />
+              </div>
+              <div className={styles.pieCompleteInnerWrapper}>
+                <span>Incomplete:</span>
+                <div className={styles.grayDot} />
+              </div>
             </div>
           </div>
         </div>
-        <div className={styles.personalScrolls}>
-          <div className={styles.pScrollHeader}>
-            <span>
-              Personal Scrolls
-            </span>
-            <div className={styles.formAndSpan}>
-              <form
-                className={styles.pScrollSelection}
-                onSubmit={(event) => filterScrolls(event, category, fakeScrolls)}
-              >
-                <select name="scrolls" id="scrolls" value={category} onChange={(e) => setCategory(e.target.value)}>
-                  <option value="all scrolls">All Scrolls</option>
-                  <option value="my scrolls">My Scrolls</option>
-                  <option value="favorited scrolls">Favorited Scrolls</option>
-                </select>
-                <button type="submit" className={`${styles.tableBtn} ${styles.filterBtn}`}>Filter</button>
-              </form>
-              <span>
-                Scrolls:&nbsp;
-                {fakeScrolls.length}
-                /50
-              </span>
-            </div>
-            <hr className={styles.hrLine} />
+      </div>
+      <hr className={styles.divider} />
+      <div className={styles.outerWrapper}>
+        <div className={styles.stickyBlocks}>
+          <div className={styles.stickyBlock}>
+            <span>Start Lesson</span>
+            <button type="submit">Start</button>
           </div>
-          <div className={styles.tableContainer}>
-            <table>
-              <thead className={styles.THeadDB}>
+          <div className={styles.stickyBlock}>
+            <span>Start Mission</span>
+            <button type="submit">Start</button>
+          </div>
+        </div>
+        <div className={styles.rightWrapper}>
+          {toggleModal && (
+          <ScrollModal
+            title={selectedScroll.title}
+            regex={selectedScroll.regex}
+            user={selectedScroll.user}
+            desc={selectedScroll.desc}
+            id={selectedScroll.id}
+            setToggleModal={setToggleModal}
+            addModal={addModal}
+            editModal={editModal}
+            scrollExists={doesScrollExist(selectedScroll.id)}
+          />
+          ) }
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Regex</th>
+                <th>User</th>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {fakeScrolls.map((scroll) => (
                 <tr>
-                  <th className={styles.THlarge}>Regex</th>
-                  <th className={styles.THsmall}>User</th>
-                  <th className={styles.THsmall}>Desc</th>
-                  <th className={styles.THsmall}>Delete</th>
+                  <td>{scroll.title}</td>
+                  <td>{scroll.regex}</td>
+                  <td>{scroll.user}</td>
+                  <td>{scroll.desc}</td>
+                  <td><button type="submit" className={styles.editBtn} onClick={() => scrollSelector(scroll)}>Edit</button></td>
+                  <td><button type="submit" className={styles.delBtn}>Delete</button></td>
                 </tr>
-              </thead>
-              <tbody>
-                {filteredScrolls.map((scroll) => (
-                  <tr key={scroll.id}>
-                    <td className={`${styles.THlarge} ${styles.tdGeneral}`}>{scroll.regex}</td>
-                    <td className={`${styles.THsmall} ${styles.tdGeneral}`}>{scroll.user}</td>
-                    <td className={`${styles.THsmall} ${styles.tdGeneral}`}>
-                      <button type="submit" className={styles.tableBtn} onClick={(event) => setupModal(event, scroll)}>
-                        Desc
-                      </button>
-                    </td>
-                    <td className={`${styles.THsmall} ${styles.tdGeneral}`}>
-                      <button type="submit" className={`${styles.tableBtn} ${styles.delBtn}`} onClick={(event) => deleteScroll(event, scroll)}>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            {toggleModal && (
-            <ScrollModal
-              regex={currentScroll.regex}
-              user={currentScroll.user}
-              desc={currentScroll.desc}
-              setToggleModal={setToggleModal}
-            />
-            )}
+              ))}
+            </tbody>
+          </table>
+          <div className={styles.addScrollWrapper}>
+            <button type="submit" className={styles.addScrollBtn} onClick>Add Scroll</button>
           </div>
         </div>
       </div>
